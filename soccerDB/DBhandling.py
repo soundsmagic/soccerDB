5cbc215aca11fecafffb65765a68cb6dd9c92d97
John Svensson <john-svensson@hotmail.com>
2021-06-25 16:54:16 +0200

    Nope. Continuing with tkinter. Changing to OOP.

--- soccerDB/DBhandling.py
+++ soccerDB/DBhandling.py
@@ -1,12 +1,17 @@
 import pyodbc
 
-conn_str = (
-    r"Driver={SQL Server Native Client 11.0};"
-    r"Server=(localdb)\MSSQLLocalDB;"
-    r"Database=C:\USERS\JOHN-\ONEDRIVE\DOKUMENT\SQL ASSIGNMENT.mdf;"
-    r"Trusted_Connection=yes;"
-)
 
-connection = pyodbc.connect(conn_str)
+class Database:
+    def __init__(self):
+        self.conn_str = (
+            r"Driver={SQL Server Native Client 11.0};"
+            r"Server=(localdb)\MSSQLLocalDB;"
+            r"Database=C:\USERS\JOHN-\ONEDRIVE\DOKUMENT\SQL ASSIGNMENT.mdf;"
+            r"Trusted_Connection=yes;"
+        )
+        self.connection = pyodbc.connect(self.conn_str)
+        self.cursor = self.connection.cursor()
 
-cursor = connection.cursor()
+    def fetchCountries(self):
+        self.cursor.execute("SELECT UNIQUE [Home Team] FROM SoccerResults")
+        return self.cursor.fetchall()
--- soccerDB/gui.py
+++ soccerDB/gui.py
@@ -1,38 +1,46 @@
 import tkinter as tk
 from tkinter.font import Font
 from PIL import ImageTk, Image
+import DBhandling
 
-# Create the GUI root
-root = tk.Tk()
-root.title("International Soccer Results Database App")
-root.iconbitmap("images/football.ico")
-root.geometry("1000x461")
 
-# Create tkinter variables
-hometeam = tk.StringVar()
-awayteam = tk.StringVar()
+class Application(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        master.title("International Soccer Results Database App")
+        master.geometry("1000x461")
+        master.iconbitmap("images/football.ico")
+        self.label_font = Font(family="Arial", size=14)
+        self.create_background()
+        self.create_labels()
 
-# Add background image
-background_image = ImageTk.PhotoImage(Image.open("images/soccer_bg.jpg"))
-background_label = tk.Label(root, image=background_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+    def create_background(self):
+        self.background_image = ImageTk.PhotoImage(Image.open("images/soccer_bg.jpg"))
+        self.background_label = tk.Label(self.master, image=self.background_image)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# Create font(s) for the GUI
-label_font = Font(family="Arial", size=14)
+    def create_labels(self):
+        self.label_hometeam = tk.Label(
+            self.master,
+            text="Choose the home team:",
+            font=self.label_font,
+            fg="white",
+            bg="#333",
+        )
+        self.label_awayteam = tk.Label(
+            self.master,
+            text="Choose the away team:",
+            font=self.label_font,
+            fg="white",
+            bg="#333",
+        )
+        self.label_hometeam.grid(row=0, column=0, padx=(50, 0), pady=(10, 0))
+        self.label_awayteam.grid(row=1, column=0, padx=(50, 0), pady=(10, 0))
 
-# Create labels
-label_hometeam = tk.Label(
-    root, text="Choose the home team:", font=label_font, fg="white", bg="#333"
-)
-label_awayteam = tk.Label(
-    root, text="Choose the away team:", font=label_font, fg="white", bg="#333"
-)
-
-# Create drop down menus
-dropdown_hometeam = tk.OptionMenu(root, hometeam, "Sweden", "Denmark")
-
-# Place labels
-label_hometeam.grid(row=0, column=0, padx=(50, 0), pady=(10, 0))
-label_awayteam.grid(row=1, column=0, padx=(50, 0), pady=(10, 0))
-
-root.mainloop()
+    def create_dropdowns(self):
+        self.hometeam = tk.StringVar()
+        self.dropdown_hometeam = tk.OptionMenu(
+            self.master, self.hometeam, "Sweden", "Denmark"
+        )
+        self.awayteam = tk.StringVar()
